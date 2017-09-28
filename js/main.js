6d37a4c943fc335d937d216035518f585ac7dc2b
Stephen Moy <hello@moymadethis.com>
2017-09-28 16:10:22 +0100

    Updated parallax js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,4 @@
 
-// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-
-// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
-
-// MIT license
-
-(function() {
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
-                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
- 
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
-              timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
- 
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
-}());
-
 /* ==========================================================================
    #NAVIGATION
    ========================================================================== */
@@ -84,20 +52,24 @@ $(function(){
  * Adds classes to specified navigation element when page is scrolled up/down.
  */
 
-$('#metropallax').parallax({
-	calibrateX: false,
-	calibrateY: true,
-	invertX: false,
-	invertY: true,
-	limitX: false,
-	//limitY: 10,
-	limitY: 0,
-	scalarX: 2,
-	scalarY: 8,
-	frictionX: 0.2,
-	frictionY: 0.8,
-	originX: 0.0,
-	originY: 1.0
+$(function(){
+	var scene = document.getElementById('metropallax');
+	var parallaxInstance = new Parallax(scene, {
+		relativeInput: true,
+		calibrateX: false,
+		calibrateY: true,
+		invertX: false,
+		invertY: true,
+		limitX: false,
+		//limitY: 10,
+		limitY: 0,
+		scalarX: 2,
+		scalarY: 8,
+		frictionX: 0.2,
+		frictionY: 0.8,
+		originX: 0.0,
+		originY: 1.0
+	});
 });
 
 
