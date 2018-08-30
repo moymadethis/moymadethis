4dc9768c52098f7ebdfd1d6bf558b6a54f2ec1e7
Stephen Moy <hello@moymadethis.com>
2018-08-30 22:18:25 +0100

    Updated whco

--- sites/twhc/js/main.js
+++ sites/twhc/js/main.js
@@ -151,7 +151,7 @@ $(document).ready(function(){
  
 $(document).ready(function() {
   $('.slick-carousel').slick({
-      centerMode: true,
+      //centerMode: true,
       centerPadding: '0',
       slidesToShow: 3,
       arrows: false,
