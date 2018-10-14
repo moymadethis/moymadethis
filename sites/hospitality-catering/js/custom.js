a14ff484e4c7d50a28ce3999ea3aa54e9c34bd99
Stephen Moy <hello@moymadethis.com>
2018-10-14 20:22:20 +0100

    Added hospitality and cateringlity and catering course for client to look at. Still needs styling and final 3 sessions addded

---
+++ sites/hospitality-catering/js/custom.js
@@ -0,0 +1,8 @@
+
+/*------------------------------------*\
+    #CUSTOM JS
+\*------------------------------------*/
+
+/**
+ * Javascript that is custom to a course ...shouldn't be any, but just incase.
+ */
