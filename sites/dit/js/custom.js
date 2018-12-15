f9a9961d71d9d1ea65fb1228f8fe06c54a5daba5
Stephen Moy <hello@moymadethis.com>
2018-12-15 11:54:28 +0000

    Removed dit course

--- sites/dit/js/custom.js
+++
@@ -1,8 +0,0 @@
-
-/*------------------------------------*\
-    #CUSTOM JS
-\*------------------------------------*/
-
-/**
- * Javascript that is custom to a course ...shouldn't be any, but just incase.
- */
